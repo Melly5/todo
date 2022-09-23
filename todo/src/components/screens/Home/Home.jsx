60c352aba0cbb7423197fcd707080484f0995d31
Melly5 <92356658+Melly5@users.noreply.github.com>
2022-09-24 00:13:26 +0700

    add addTodo, removeTodo
    smth wrong with remove function

--- todo/src/components/screens/Home/Home.jsx
+++ todo/src/components/screens/Home/Home.jsx
@@ -1,33 +1,50 @@
 import React, { useState } from "react";
+import { CreateTodo } from "./create-todo/CreateTodo";
 import { TodoItem } from "./item/TodoItem";
 
 const data = [
   {
-    _id: 'esfes223',
-    title: 'Finish the essay',
+    _id: "esfes223",
+    title: "Finish the essay",
     isCompleted: false,
   },
   {
-    _id: 'esfes2dfd23',
-    title: 'read next chapter',
+    _id: "esfes2dfd23",
+    title: "read next chapter",
     isCompleted: false,
   },
   {
-    _id: 'esddsfes223',
-    title: 'Finish the essay',
+    _id: "esddsfes223",
+    title: "Finish the essay",
     isCompleted: false,
   },
 ];
 
 export const Home = () => {
+  const [todos, setTodos] = useState(data);
+
+  const changeTodo = (id) => {
+    const copy = [...todos];
+    const current = copy.find((t) => t._id === id);
+    current.isCompleted = !current.isCompleted;
+    setTodos(copy);
+  };
+
+  const removeTodo = id => setTodos([...todos].filter(t => t._id !== id));
+
 
   return (
     <div className=" text-white w-4/5 mx-auto">
       <h1 className="text-2xl font-bold text-center mb-10">TODO</h1>
-      {todos.map(todo => (
-        <TodoItem key={todo._id} todo={todo}/>
+      {todos.map( todo => (
+        <TodoItem
+          key={todo._id}
+          todo={todo}
+          changeTodo={changeTodo}
+          removeTodo={removeTodo}
+        />
       ))}
+      <CreateTodo setTodos={setTodos}/>
     </div>
   );
 };
-
--- todo/src/components/screens/Home/item/TodoItem.jsx
+++ todo/src/components/screens/Home/item/TodoItem.jsx
@@ -1,12 +1,30 @@
-import React from "react";
-import { Check } from "./Check";
+import React from "react"
+import { Check } from "./Check"
+import { BsTrash } from "react-icons/bs"
+import cn from "classnames"
 
-export const TodoItem = ({ todo }) => {
+export const TodoItem = ({ todo, changeTodo, removeTodo }) => {
   return (
-    <button className="flex items-center mb-4 rounded-2xl bg-gray-800 p-5 w-full">
-      <Check/>
-      {todo.title}
-    </button>
+    <div
+      className="flex items-center justify-between mb-4 rounded-2xl bg-gray-800 p-5 w-full"
+      onClick={() => changeTodo(todo._id)}
+    >
+      <button className="flex items-center">
+        <Check isCompleted={todo.isCompleted} />
+        <span
+          className={cn({
+            "line-through": todo.isCompleted,
+          })}
+        >
+          {todo.title}
+        </span>
+      </button>
+      <button onClick={() => removeTodo(todo._id)}>
+        <BsTrash
+          size={22}
+          className="text-gray-600 hover:text-red-700 transition-colors ease-in-out duration-300"
+        />
+      </button>
+    </div>
   );
 };
-
